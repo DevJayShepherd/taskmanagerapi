f29be2f912635e4cb793d2768573ffa8495a141a
jay <jay.shepherd@affinityship.com>
2025-01-17 11:44:09 +0000

    Add dotenv support and basic FastAPI configuration
    Introduced `python-dotenv` to manage environment variables and added a `.env-example` file for reference. Updated FastAPI app with middleware setup from environment-based settings. Created `.gitignore` to exclude sensitive and unnecessary files from version control.

---
+++ taskmanager/config.py
@@ -0,0 +1,26 @@
+# Core
+import os
+
+# Imports
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+class Settings:
+    PROJECT_TITLE: str = "Task Manager API"
+    PROJECT_VERSION: str = "0.1.0"
+    ENVIRONMENT: str = os.getenv('ENVIRONMENT')
+
+    # Settings for domain and protocol
+    API_DOMAIN: str = os.getenv("API_DOMAIN")
+    API_PROTOCOL: str = os.getenv("API_PROTOCOL")
+    BASE_URL = f"{API_PROTOCOL}://{API_DOMAIN}"
+    CORS_ORIGINS: list = os.getenv("CORS_ORIGINS", "").split(',')
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+settings = Settings()
--- taskmanager/main.py
+++ taskmanager/main.py
@@ -1,8 +1,29 @@
+# Core FastAPI application
 from fastapi import FastAPI
 
-app = FastAPI()
+# Middlewares
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 
-@app.get("/")
-async def root():
-    return {"message": "Task Manager API"}
+def include_middlewares(app):
+    app.add_middleware(GZipMiddleware, minimum_size=1000)  # Add GZipMiddleware
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.CORS_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )  # TODO - restrict origins
+    app.add_middleware(SessionMiddleware, secret_key="some-random-string")
+
+    return app
+
+
+def start_api():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    app = include_middlewares(app)
+    return app
+
+api = start_api()
